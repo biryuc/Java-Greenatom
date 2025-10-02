5f38b682bc70ea1bf61e073be66a66a0b04f8f30
biryuc <nikitabiryuchkov@icloud.com>
2025-10-02 19:04:47 +0300

    Task 4 main

--- src/Task4/Main.java
+++ src/Task4/Main.java
@@ -2,16 +2,22 @@ package Task4;
 
 public class Main {
     public static void main(String[] args) {
-        ObservableStringBuilder osb = new ObservableStringBuilder("Hello");
+        ObservableStringBuilder osb = new ObservableStringBuilder("Рукописи");
 
         // Добавляем наблюдателя
-        osb.addObserver(newValue -> System.out.println("StringBuilder изменен: " + newValue));
+        osb.addObserver(newValue -> System.out.println("Изменение: " + newValue));
 
-        // Проводим изменения
-        osb.append(" World");
-        osb.insert(5, ",");
-        osb.replace(0, 5, "Hi");
-        osb.delete(2, 3);
+        // Делаем изменения
+        osb.append(" не");
+        osb.append(" горят");
+
+        // Попробуем заменить часть строки
+        osb.replace(0, 9, "Рукописи всё же");
+
+        // Удалим добавленное " всё же"
+        osb.delete(9, 15);
+
+        // Для красоты развернём строку
         osb.reverse();
     }
 }
